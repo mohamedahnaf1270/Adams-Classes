7a55151ae54ea89f00b85f52e2a651189d022a1b
mohamedahnaf1270 <mohamedahu2017@gmail.com>
2023-01-25 22:26:58 +0500

    first commit

---
+++ Advanacedinstallations.php
@@ -0,0 +1,24 @@
+<div class="container pt-5">
+            <h1>Advanced Installations</h1>
+            <hr>
+    <table class="table table-dark table-striped pb-2">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Type</th>
+      <th scope="col">Information</th>
+      <th scope="col">Links</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row" class="p-3">1</th>
+      <td class="p-3">Xampp</td>
+      <td class="p-3">Web Server</td>
+      <td class="p-3">XAMPP is a free and open-source cross-platform web server solution stack package developed by Apache Friends, consisting mainly of the Apache HTTP Server, MariaDB database, and interpreters for scripts written in the PHP and Perl programming languages</td>
+      <td class="p-3"><a href="https://www.apachefriends.org/download.html">Download</a></td>
+    </tr>
+  </tbody>
+</table>
+</div>
